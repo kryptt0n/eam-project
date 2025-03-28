e24a9501ce84289ce9017b944e7df06f39975109
Vitaly Sukhinin <vitaliksuhinin99@gmail.com>
2025-03-28 19:11:41 -0400

    order model and repository

---
+++ order-service/src/main/java/com/eam/order_service/Order.java
@@ -0,0 +1,54 @@
+package com.eam.order_service;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Order {
+    @Id
+    private String id;
+    private Integer count;
+    private String stock;
+    private String account;
+
+    public Order() {
+    }
+
+    public Order(Integer count, String stock, String account) {
+        this.count = count;
+        this.stock = stock;
+        this.account = account;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    public String getStock() {
+        return stock;
+    }
+
+    public void setStock(String stock) {
+        this.stock = stock;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+}
